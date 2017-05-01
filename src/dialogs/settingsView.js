54d2e57c8343fab1510833bc2a4de533785c14c1
Lasse Diercks <lasse@jimdo.com>
2017-05-01 22:31:27 +0200

    feat(settings): introduce some style

--- src/dialogs/settingsView.js
+++ src/dialogs/settingsView.js
@@ -7,19 +7,23 @@ const formatTime = require('../util/formatTime');
 
 function checkBoxWidget(property) {
   const options = {
+    className: 'sd-checkbox__input',
     checked: config[property].value,
     onclick: m.withAttr('checked', () => {
       config[property].value = !config[property].value;
     })
   };
-  return m('span', [
-    m('label', `${config[property].label}`),
-    m('input[type="checkbox"]', options)
+  return m('label', {className: 'sd-checkbox'},[
+    m('input[type="checkbox"]', options),
+    m('span', { className: 'sd-checkbox__text'}, `${config[property].label}`)
   ]);
 }
 
+
+
 function rangeWidget(property) {
   const options = {
+    className: 'sd-range__input',
     min: 1,
     step: 1,
     max: 2700,
@@ -28,20 +32,24 @@ function rangeWidget(property) {
       config[property].value = event.target.value;
     }
   };
-  return [
-    m('label', `${config[property].label}: ${formatTime(config[property].value)}s`),
+  return m('label', {className: 'sd-range'}, [
+    m('span', {className: 'sd-range__label'}, `${config[property].label}: ${formatTime(config[property].value)}s`),
     m('input[type="range"]', options)
-  ];
+  ]);
 }
 
 module.exports = function(scope) {
   return m('aside', {
+    className: 'sd-modal',
     'data-dialog-open': scope.settingDialogOpen.toString()
-  }, [
-    m('h2', 'Settings'),
-    checkBoxWidget('silentNotification'),
-    rangeWidget('pomodoroDuration'),
-    rangeWidget('shortBreakDuration'),
-    rangeWidget('longBreakDuration')
-  ]);
+  }, m('div', {
+      className: 'sd-modal__content'
+    }, [
+      m('h2', {className: 'sd-headline'} ,'Settings'),
+      checkBoxWidget('silentNotification'),
+      rangeWidget('pomodoroDuration'),
+      rangeWidget('shortBreakDuration'),
+      rangeWidget('longBreakDuration')
+    ])
+  );
 };
